c72d94dd1cd49728d5e07e3671f82c974cfb86ac
Martin Halamíček <martin@halamicek.cz>
2015-12-16 15:40:41 +0100

    link

--- codedeploy-scripts/link.sh
+++ codedeploy-scripts/link.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+# move symlink to current
 mkdir /var/www/$DEPLOYMENT_ID && cp -a /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/* /var/www/$DEPLOYMENT_ID
+rm -f /var/www/current && ln -s /var/www/$DEPLOYMENT_ID /var/www/current
 exit $?
