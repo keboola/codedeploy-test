ac13074db6500472b2521bdef3fd7ccb459df483
Martin Halamíček <martin@halamicek.cz>
2015-12-16 16:05:36 +0100

    donwload config params

--- codedeploy-scripts/download-config.sh
+++ codedeploy-scripts/download-config.sh
@@ -1,3 +1,30 @@
 #!/bin/bash
-aws s3 cp s3://codedeploy-test-martin/index.tpl /var/www/$DEPLOYMENT_ID/config.ini
-exit $?
+
+# Usage: error_exit <message>
+#
+#   Writes <message> to STDERR as a "fatal" and immediately exits the currently running script.
+error_exit() {
+    local message=$1
+
+    echo "[FATAL] $message" 1>&2
+    exit 1
+}
+
+
+if [ "$DEPLOYMENT_GROUP_NAME" == "Production" ]
+then
+  aws s3 cp s3://codedeploy-test-martin/index.tpl /var/www/$DEPLOYMENT_ID/config.ini
+  if [ $? != 0 ]; then
+    error_exit "Failed to download S3 parameters"
+  fi
+elif [ "$DEPLOYMENT_GROUP_NAME" == "Production" ]
+then
+  aws s3 cp s3://codedeploy-test-martin/index.tpl /var/www/$DEPLOYMENT_ID/config.ini
+  if [ $? != 0 ]; then
+    error_exit "Failed to download S3 parameters"
+  fi
+else
+  error_exit "Unsupportd deployment group $DEPLOYMENT_GROUP_NAME"
+fi
+
+exit 0
