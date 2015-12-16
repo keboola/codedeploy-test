b18a7d012f0f891efc5bd47c18b9ce726ebfb0c8
Martin Halamíček <martin@halamicek.cz>
2015-12-16 14:57:43 +0100

    fix - invalid chars

--- codedeploy-scripts/prepare.sh
+++ codedeploy-scripts/prepare.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-mkdir /var/www/$DEPLOYMENT_ID && cp -R /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/ /var/www/$DEPLOYMENT_ID
+mkdir /var/www/$DEPLOYMENT_ID && cp -R /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/ /var/www/$DEPLOYMENT_ID
 exit $?
